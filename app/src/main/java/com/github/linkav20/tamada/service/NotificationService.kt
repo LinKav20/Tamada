2226728505d9fe493550def1f66794a10e00207b
LinKav20 <linok2111@gmail.com>
2024-05-05 23:51:37 +0300

    add draft firebase cloud messaging

---
+++ app/src/main/java/com/github/linkav20/tamada/service/NotificationService.kt
@@ -0,0 +1,52 @@
+package com.github.linkav20.tamada.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.github.linkav20.tamada.R
+import com.github.linkav20.tamada.presentation.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val CHANNEL_ID = "NOTIFICATIONS"
+const val CHANNEL_NAME = "com.github.linkav20.tamada.notifications"
+
+class NotificationService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationId = 1
+        val requestCode = 1
+
+        val channelId = CHANNEL_ID
+        val channelName = CHANNEL_NAME
+        notificationManager.createNotificationChannel(
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+        )
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntentFlag = PendingIntent.FLAG_IMMUTABLE
+        val pendingIntent = PendingIntent.getActivity(this, requestCode, intent, pendingIntentFlag)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(message.notification?.title)
+            .setContentText(message.notification?.body)
+            .setSmallIcon(R.drawable.splash)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(notificationId, notification)
+    }
+
+}
